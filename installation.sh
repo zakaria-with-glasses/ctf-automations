7ff12df2f685df08f6f64d1e186e4191359264da
Zakaria HARIRA <64852361+zakaria-with-glasses@users.noreply.github.com>
2025-09-29 10:01:14 +0100

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,7 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y git curl wget unzip build-essential python3 python3-pip python3-venv net-tools
+sudo apt install -y nmap netcat-traditional dnsutils whois curl wget
+sudo apt install -y metasploit-framework sqlmap hydra
+sudo apt install -y burpsuite
+sudo apt install -y binwalk foremost steghide exiftool strings
+pip3 install pwntools
